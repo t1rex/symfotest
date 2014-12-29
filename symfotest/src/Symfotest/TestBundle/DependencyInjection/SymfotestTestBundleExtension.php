4034c2181f6441b1308163de438ff8c54af729d4
Nazar <nazarp@default-value.com>
2014-12-29 17:17:54 +0200

    Added example control panel

---
+++ symfotest/src/Symfotest/TestBundle/DependencyInjection/SymfotestTestBundleExtension.php
@@ -0,0 +1,14 @@
+<?php
+namespace Symfotest\TestBundle\DependencyInjection;
+
+use Symfony\Component\DependencyInjection\Loader;
+use Symfony\Component\Config\FileLocator;
+
+class AcmeDemoBundleExtension extends Extension
+{
+public function load(array $configs, ContainerBuilder $container) {
+// ...
+$loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+$loader->load('admin.yml');
+}
+}
